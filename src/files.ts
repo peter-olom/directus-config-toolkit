62f513f8764b1dc508c08e177eba181e38bae908
Peter Olom <peter@olom.dev>
2025-01-24 15:50:21 +0100

    chore: enhance the backup process by first confirming the presence of the backup field to avoid permissions error

--- src/files.ts
+++ src/files.ts
@@ -2,6 +2,7 @@ import {
   createFolder,
   deleteFiles,
   deleteFolders,
+  readFieldsByCollection,
   readFiles,
   readFolders,
   updateFolder,
@@ -23,28 +24,30 @@ export class FilesManager {
   private folderPath: string = join(CONFIG_PATH, "folders.json");
   private assetPath: string = join(CONFIG_PATH, "assets");
   private immutableFields = ["filename_disk", "filename_download"];
-  private shouldBackupFilter = {
-    _or: [{ shouldBackup: { _eq: true } }, { should_backup: { _eq: true } }],
-  };
-
-  private isMarkedForBackup = (item: Record<string, any>) => {
-    return item.shouldBackup === true || item.should_backup === true;
-  };
+  private backupField: string | null = null;
 
   constructor() {}
 
-  untrackIgnoredFields = (record: Record<string, any>) => {
+  private getBackupField = async (
+    collection: "directus_files" | "directus_folders"
+  ) => {
+    const result = await client.request(readFieldsByCollection(collection));
+    const backupField = result.find(({ field }) => {
+      return field === "shouldBackup" || field === "should_backup";
+    });
+    this.backupField = backupField ? backupField.field : null;
+  };
+
+  private getBackupFilter = () => {
+    return this.backupField ? { [this.backupField]: { _eq: true } } : {};
+  };
+
+  private untrackIgnoredFields = (record: Record<string, any>) => {
     const baseFields = ["id", "title", "type", "folder"];
-    const backupField =
-      "shouldBackup" in record
-        ? "shouldBackup"
-        : "should_backup" in record
-        ? "should_backup"
-        : null;
 
     return _.pick(record, [
       ...baseFields,
-      ...(backupField ? [backupField] : []),
+      ...(this.backupField ? [this.backupField] : []),
       ...this.immutableFields,
     ]);
   };
@@ -60,49 +63,6 @@ export class FilesManager {
     }
     throw error;
   }
-
-  exportFiles = async () => {
-    ensureConfigDirs();
-    // backup folders first - only those with shouldBackup set to true
-    const folders = await this.getRemoteFolders();
-    writeFileSync(this.folderPath, JSON.stringify(folders, null, 2));
-
-    // backup files next
-    let files: Record<string, any>[] = [];
-    try {
-      files = await client.request(
-        readFiles({ filter: this.shouldBackupFilter })
-      );
-    } catch (error) {
-      files = await this.handleFieldPermissionError(
-        error,
-        "Error reading files with backup filter"
-      );
-    }
-
-    writeFileSync(
-      this.filePath,
-      JSON.stringify(files.map(this.untrackIgnoredFields), null, 2)
-    );
-
-    // Download and backup files concurrently with retries
-    const downloadPromises = files.map((file) => downloadFile(file));
-    await Promise.all(downloadPromises);
-
-    console.log(`Files exported to ${this.filePath}`);
-    console.log(`Folders exported to ${this.folderPath}`);
-    console.log(`Assets exported to ${this.assetPath}`);
-  };
-
-  importFiles = async () => {
-    // import folders first
-    await this.handleImportFolders();
-    console.log("Folders imported successfully.");
-
-    // import files next
-    await this.handleImportFiles();
-    console.log("Files imported successfully.");
-  };
 
   private handleImportFolders = async () => {
     const sourceFolders: Record<string, any>[] = JSON.parse(
@@ -132,7 +92,7 @@ export class FilesManager {
       readFileSync(this.filePath, "utf8")
     );
     const destinationFiles = await client.request(
-      readFiles({ filter: this.shouldBackupFilter })
+      readFiles({ filter: this.getBackupFilter() })
     );
 
     const filePromises = sourceFiles.map(async (file) => {
@@ -198,7 +158,7 @@ export class FilesManager {
       // Get folders explicitly marked for backup
       markedFolders = await client.request(
         readFolders({
-          filter: this.shouldBackupFilter,
+          filter: this.getBackupFilter(),
         })
       );
     } catch (error) {
@@ -213,7 +173,7 @@ export class FilesManager {
       files = await client.request(
         readFiles({
           filter: {
-            _and: [this.shouldBackupFilter, { folder: { _nnull: true } }],
+            _and: [this.getBackupFilter(), { folder: { _nnull: true } }],
           },
         })
       );
@@ -248,4 +208,53 @@ export class FilesManager {
     // Combine and deduplicate folders
     return _.uniqBy([...markedFolders, ...relatedFolders], "id");
   }
+
+  exportFiles = async () => {
+    ensureConfigDirs();
+
+    // backup folders first - only those with should backup set to true
+    await this.getBackupField("directus_folders");
+    const folders = await this.getRemoteFolders();
+    writeFileSync(this.folderPath, JSON.stringify(folders, null, 2));
+
+    // backup files next
+    await this.getBackupField("directus_files");
+    // backup files next
+    let files: Record<string, any>[] = [];
+    try {
+      files = await client.request(
+        readFiles({ filter: this.getBackupFilter() })
+      );
+    } catch (error) {
+      files = await this.handleFieldPermissionError(
+        error,
+        "Error reading files with backup filter"
+      );
+    }
+
+    writeFileSync(
+      this.filePath,
+      JSON.stringify(files.map(this.untrackIgnoredFields), null, 2)
+    );
+
+    // Download and backup files concurrently with retries
+    const downloadPromises = files.map((file) => downloadFile(file));
+    await Promise.all(downloadPromises);
+
+    console.log(`Files exported to ${this.filePath}`);
+    console.log(`Folders exported to ${this.folderPath}`);
+    console.log(`Assets exported to ${this.assetPath}`);
+  };
+
+  importFiles = async () => {
+    // import folders first
+    await this.getBackupField("directus_folders");
+    await this.handleImportFolders();
+    console.log("Folders imported successfully.");
+
+    // import files next
+    await this.getBackupField("directus_files");
+    await this.handleImportFiles();
+    console.log("Files imported successfully.");
+  };
 }
